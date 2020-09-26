fc31db4ba3813b25da17770e710b358513882213
nikhilmedappa <nikhilmedappa@gmail.com>
2020-09-26 11:51:46 +0530

    Music Category Added

---
+++ examples/node/minimal-usage.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var faker = require('../../index');
+faker.locale = "en";
+
+
+// for(var i=0;i<2;i++)
+// {
+console.log(faker.music.getGenre())
+console.log(faker.music.getBpm())
+console.log(faker.music.getArtistname())
+// }
+
+return;
